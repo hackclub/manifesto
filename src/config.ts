b1e1e6c14883d130513de936a86d5143080bac73
techpixel <68567672+techpixel@users.noreply.github.com>
2026-03-06 13:55:52 -0500

    fix: unfurl

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,15 @@
 export const APPLY_URL = "https://forms.hackclub.com/t/isRvULMQR3us";
+export const SITE_URL = "https://hackclub.com/gap-year";
+
+export const OG = {
+  index: {
+    title: "Hackers Wanted.",
+    description: "Hack Club is hiring 40 teenagers on a paid gap year to build the next generation of Hack Club’s programs.",
+    image: "/og.png",
+  },
+  manifesto: {
+    title: "Hackers Wanted. - a manifesto",
+    description: "Hack Club is hiring 40 teenagers on a paid gap year to build the next generation of Hack Club’s programs.",
+    image: "/og.png",
+  },
+};
